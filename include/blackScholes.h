b56d152078d02969ae451fd1e283cfd606c740df
Saadan Naqvi <saadannaqvi@Saadans-MacBook-Pro.local>
2025-08-19 15:18:57 +0930

    setting up black scholes class

--- include/blackScholes.h
+++ include/blackScholes.h
@@ -1,5 +1,5 @@
-#ifndef CALCULATOR
-#define CALCULATOR
+#ifndef BLACK_SCHOLES
+#define BLACK_SCHOLES
 
 #include <cmath>
 using namespace std;
@@ -12,6 +12,7 @@ class BlackScholes{
         double riskFreeInterestRate;
         double volatilityOfUnderlyingAsset;
         double dividendYield;
+        // Capture the combined effect of time, volatility, and drift on the option payoff distribution.
         double d1 = (log(spotPrice/strikePirce)+(riskFreeInterestRate-dividendYield+(0.5)*(pow(volatilityOfUnderlyingAsset,2))))/(volatilityOfUnderlyingAsset*(sqrt(timeToMaturity)));
         double d2 = d1-volatilityOfUnderlyingAsset*sqrt(timeToMaturity);
 };
