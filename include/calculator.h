c76334cd5b2d9055a362c910c4ceb829497db45b
Saadan Naqvi <saadannaqvi@Saadans-MacBook-Pro.local>
2025-08-19 15:11:49 +0930

    Setting stuff up

---
+++ include/calculator.h
@@ -0,0 +1,19 @@
+#ifndef CALCULATOR
+#define CALCULATOR
+
+#include <cmath>
+using namespace std;
+
+class BlackScholes{
+    public:
+        double spotPrice;
+        double strikePirce;
+        double timeToMaturity;
+        double riskFreeInterestRate;
+        double volatilityOfUnderlyingAsset;
+        double dividendYield;
+        double d1 = (log(spotPrice/strikePirce)+(riskFreeInterestRate-dividendYield+(0.5)*(pow(volatilityOfUnderlyingAsset,2))))/(volatilityOfUnderlyingAsset*(sqrt(timeToMaturity)));
+        double d2 = d1-volatilityOfUnderlyingAsset*sqrt(timeToMaturity);
+};
+
+#endif
---
+++ include/types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES
+#define TYPES
+
+using namespace std;
+
+class Sigma{
+    public:
+        int myNum;
+    
+};
+
+#endif
