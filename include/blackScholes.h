3547b31352c933d1e2e5501bd9ce6cbd06ff10f0
Saadan Naqvi <saadannaqvi@Saadans-MacBook-Pro.local>
2025-08-25 09:45:27 +0930

    IMgui lib

--- include/blackScholes.h
+++ include/blackScholes.h
@@ -5,16 +5,15 @@
 using namespace std;
 
 class BlackScholes{
-    public:
-        double spotPrice;
-        double strikePirce;
-        double timeToMaturity;
-        double riskFreeInterestRate;
-        double volatilityOfUnderlyingAsset;
-        double dividendYield;
-        // Capture the combined effect of time, volatility, and drift on the option payoff distribution.
-        double d1 = (log(spotPrice/strikePirce)+(riskFreeInterestRate-dividendYield+(0.5)*(pow(volatilityOfUnderlyingAsset,2))))/(volatilityOfUnderlyingAsset*(sqrt(timeToMaturity)));
-        double d2 = d1-volatilityOfUnderlyingAsset*sqrt(timeToMaturity);
+    double spotPrice;
+    double strikePirce;
+    double timeToMaturity;
+    double riskFreeInterestRate;
+    double volatilityOfUnderlyingAsset;
+    double dividendYield;
+    // Capture the combined effect of time, volatility, and drift on the option payoff distribution.
+    double d1 = (log(spotPrice/strikePirce)+(riskFreeInterestRate-dividendYield+(0.5)*(pow(volatilityOfUnderlyingAsset,2))))/(volatilityOfUnderlyingAsset*(sqrt(timeToMaturity)));
+    double d2 = d1-volatilityOfUnderlyingAsset*sqrt(timeToMaturity);
 };
 
 #endif
